45d52f24470357b7f554dfb80f15ddcf14b0f1c5
Melih Çağan Arı <melihcaganari@gmail.com>
2025-02-27 21:15:55 +0300

    Last Update

--- Assignment2/portfolio/tailwind.config.js
+++ Assignment2/portfolio/tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        parkinsans: ['Parkinsans', 'sans-serif'],
+        cinzel: ['Cinzel', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
